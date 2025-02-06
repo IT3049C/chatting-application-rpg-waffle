748d576eb6f4ba956816b6d38bfcc1e8e00c5fba
Chris Allen <allen5ch@mail.uc.edu>
2025-02-06 14:59:12 -0500

    references created

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,10 @@
 // Getting Messages:
 
-  // Creating References: 
+  // Creating References:
+  const myName = document.getElementById(`my-name-input`);
+  const messageInput = document.getElementById(`my-message`);
+  const sendButton = document.getElementById(`send-button`);
+  const chatBox = document.getElementById(`chat`); 
 
   // Function formatMessage(): 
 
