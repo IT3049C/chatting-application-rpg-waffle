61c46308e57183c3ee305eb52a369f95418ad2bf
Chris Allen <allen5ch@mail.uc.edu>
2025-02-06 14:54:25 -0500

    comment out javascript

---
+++ resources/js/index.js
@@ -0,0 +1,15 @@
+// Getting Messages:
+
+  // Creating References: 
+
+  // Function formatMessage(): 
+
+  // Function fetchMessages():
+
+  // Function updateMessagesInChatBox():
+
+// Sending Messages:
+
+  // Function send
+
+  // Send Button Event Listener
